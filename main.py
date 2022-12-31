fa1c9b1ebe8c0ebbe5b4c467363ba7494873acb7
Kaleb Fenley <99980282+ImKaleb@users.noreply.github.com>
2022-12-31 01:24:52 -0600

    Update main.py
    made a bit more 'pythonic'

--- main.py
+++ main.py
@@ -6,23 +6,28 @@ import csv
 #############################################
 FILEPATH = 'C:\\tmp\\'                      #
 FILENAME = 'results.csv'                    #
-FULL_PATH = FILEPATH+FILENAME               #
+FULL_PATH = FILEPATH + FILENAME             #
 #############################################
 
+def main():
+    with requests.Session() as session:
+        url = 'https://taxsales.lgbs.com/api/property_sales/?limit=10000'
+        print('\n\n-- SEARCHING FOR TAX SALE PROPERTIES --')
+        results = session.get(url)
+        content = results.json()
+        property_list = content['results']
+        print('\n\n-- PROPERTIES GATHERED --')
+        print('\n\n-- WRITING RESULTS TO CSV FILE --')
+        write_to_csv(property_list, FULL_PATH)
+        print(f'-- FINISHED WRITING "{FULL_PATH}" --')
+        print('-- PROGRAM COMPLETE --')
 
-with requests.Session() as SESSION:
-    print('\n\n-- SEARCHING FOR TAX SALE PROPERTIES --')
-    URL = 'https://taxsales.lgbs.com/api/property_sales/?limit=10000'
-    RESULTS = SESSION.get(URL)
-    CONTENT = RESULTS.json()
-    PROPERTY_LIST = CONTENT['results']
-    print('\n\n-- PROPERTIES GATHERED --')
-    print('\n\n-- WRITING RESULTS TO CSV FILE --')
-    with open(FULL_PATH,'w')as FILE:
-        WRITER = csv.writer(FILE)
-        WRITER.writerow(PROPERTY_LIST[1].keys())
-        for DICTIONARY in PROPERTY_LIST:
-            WRITER.writerow(DICTIONARY.values())
-        FILE.close()
-    print(f'-- FINISHED WRITING "{FULL_PATH}" --')
-    print('-- PROGRAM COMPLETE --')
+def write_to_csv(property_list, full_path):
+    with open(full_path, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(property_list[1].keys())
+        for dictionary in property_list:
+            writer.writerow(dictionary.values())
+
+if __name__ == '__main__':
+    main()
